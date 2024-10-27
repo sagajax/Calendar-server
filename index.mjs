bbbdb2ded66a953da1cfa54c0d7f9f0038c708c1
sagajax <129790355+sagajax@users.noreply.github.com>
2024-10-27 23:06:42 +0530

    changes

--- index.mjs
+++ index.mjs
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.set('trust proxy', 1);
-const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://calendar-client-7g5n.vercel.app'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : ['http://localhost:5173', 'https://calendar-client-7g5n.vercel.app', "https://calendar-client-teal.vercel.app"];
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
